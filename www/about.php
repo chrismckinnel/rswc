82b30fcb3b7dac0fac4260cd541459fc6ea4edc7
Chris McKinnel <chris.mckinnel@tangentlabs.co.uk>
2013-03-12 09:32:51 +1300

    Add photos and blurbs to about

--- www/about.php
+++ www/about.php
@@ -4,7 +4,7 @@
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
 <title>About VCS West Coast Resource Consents</title>
 <link href="styles.css" rel="stylesheet" type="text/css" />
-<meta name="description" content="business description for search engines" />
+<meta name="description" content="VCS will manage the consent process for you, as well as any ongoing monitoring." />
 <meta name="keywords" content="Resource consent, west coast, compliance, environmental management" />
 <!--below is the code to display the menu properly in ie6-->
 <!--[if IE 6]>
@@ -34,9 +34,41 @@ margin:8px 25px 0px 0px; }/* you may need to change these margins if your contac
         <div id="left_column">
           <!--start main text-->
         <h1>About VCS West Coast Resource Consents</h1>
-        <p><img src="images/photo.jpg" alt="Photo" width="250" height="166" border="0" align="right" class="image_border" />
-          VCS is a West Coast business specialising in Pest Control and Environmental Management. Our team has a long history of dealing with RMA consenting issues, and have developed strong working relationships with most agencies and interest groups up and down the West Coast.<br><br>
-          Consultation is a speciality of VCS and we pride ourselves on developing innovative solutions that allow everyone to enjoy the benefits of the West Coast’s natural resources.</p>
+        <p>
+          <img src="images/phil.jpg" alt="Photo" width="230" height="180" border="0" align="right" class="image_border" />
+        </p>
+        <h2>Phil McKinnel</h2>
+        <p>
+          Phil holds a BSC and BCom from the University of Otago and has extensive experience in monitoring a 
+          large range of mining operations on the West Coast. Phil was previously employed as the Compliance 
+          Team Leader at the West Coast Regional Council.
+        </p>
+        <p>
+          Phil specialises in large scale mining operations and compliance support. 
+        </p>
+        <br><br>
+        <p>
+          <img src="images/tom.jpg" alt="Photo" width="230" height="153" border="0" align="left" class="image_border" style="margin-right: 10px;" />
+        </p>
+        <h2>Tom Overton</h2>
+        <p>
+          Tom holds a BSc from the University of Otago and has previously been employed by OPUS, Otago Regional 
+          Council and Department of Conservation. Tom has expertise in mining permit applications, natural 
+          hazard analysis and consent applications for mining operations and dairy effluent systems.
+        </p>
+        <p>
+          Tom is currently working towards being an accredited designer of effluent systems.
+        </p><br><br>
+        <p>
+          <img src="images/luke.jpg" alt="Photo" width="230" height="201" border="0" align="right" class="image_border" />
+        </p>
+        <h2>Luke McNeish</h2>
+        <p>
+          Luke has extensive experience in the dairy industry. He has previously worked as a Dairy Compliance 
+          Officer with the West Coast Regional Council and has an excellent understanding of local conditions and rules.
+        </p>
+        <p>
+          Luke is a born and bred West Coaster and is currently working towards becoming an accredited effluent system designer.
         </p>
       </div>
         <!--end left column-->
