1a903541a44ca9681c158f6cceb57ffca6c9a8c3
Chris McKinnel <chris.mckinnel@tangentlabs.co.uk>
2013-03-12 23:47:55 +1300

    Remove extra tracking codes

--- www/contact.php
+++ www/contact.php
@@ -133,18 +133,5 @@ margin:8px 25px 0px 0px; }/* you may need to change these margins if your contac
     ?>
   </div>
 </div>
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-39089683-1']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
 </body>
 </html>
